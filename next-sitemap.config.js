528756964aa1e29835fda0d7930135e0a604d93d
Pushpal Ghoshal <1804252@kiit.ac.in>
2023-04-21 18:48:59 +0530

    feat: required configs for actions and workflows

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,9 +5,9 @@
 module.exports = {
   // !STARTERCONF Change the siteUrl
   /** Without additional '/' on the end, e.g. https://trademarkia.com */
-  siteUrl: "https://trademarkia.com",
+  siteUrl: 'https://trademarkia.com',
   generateRobotsTxt: true,
   robotsTxtOptions: {
-    policies: [{ userAgent: "*", allow: "/" }],
+    policies: [{ userAgent: '*', allow: '/' }],
   },
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import * as React from "react";
+import * as React from 'react';
 
-import Layout from "@/components/layout/Layout";
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
 
-import Seo from "@/components/Seo";
-
-import Vercel from "~/svg/Vercel.svg";
+import Vercel from '~/svg/Vercel.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
@@ -17,26 +16,26 @@ export default function HomePage() {
       <Seo />
 
       <main>
-        <section className="bg-white">
-          <div className="layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center">
-            <Vercel className="text-5xl" />
-            <h1 className="mt-4 text-blue-600">Legalforce RAPC Worldwide </h1>
-            <h1 className="mt-4">
+        <section className='bg-white'>
+          <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
+            <Vercel className='text-5xl' />
+            <h1 className='mt-4 text-blue-600'>Legalforce RAPC Worldwide </h1>
+            <h1 className='mt-4'>
               Next.js + Tailwind CSS + TypeScript Template
             </h1>
-            <p className="mt-2 text-sm text-gray-800">
+            <p className='mt-2 text-sm text-gray-800'>
               A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-              Import, Seo, Link component, pre-configured with Husky{" "}
+              Import, Seo, Link component, pre-configured with Husky{' '}
             </p>
-            <p className="mt-2 text-sm text-gray-700"></p>
+            <p className='mt-2 text-sm text-gray-700'></p>
 
-            <footer className="absolute bottom-2 text-gray-700">
-              © {new Date().getFullYear()} By{" "}
+            <footer className='absolute bottom-2 text-gray-700'>
+              © {new Date().getFullYear()} By{' '}
               <a
-                href="https://pushpalghoshal.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-600"
+                href='https://pushpalghoshal.com'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-blue-600'
               >
                 Pushpal Ghoshal
               </a>
